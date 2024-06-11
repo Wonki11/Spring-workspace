2f56696297cdcc8fa8ee8656768f07cec013c501
Wonki11 <wongi5011@daum.net>
2024-06-11 14:51:05 +0900

    lombok setting, RequestMapping, Getmapping

--- demoProject2/src/main/java/com/example/demo/model/dto/MemberDTO.java
+++ demoProject2/src/main/java/com/example/demo/model/dto/MemberDTO.java
@@ -1,16 +1,22 @@
 package com.example.demo.model.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
 /*
  * Getter와 Setter 는 Lombok 라이브러리 활용해서 
  * 길게 작성하지 않음 
  * 기본생성자 
  * 
  * */
-//@Getter 
-//@Setter
-//@NoArgsConstructor
-//@AllArgsConstructor
-//@ToString
+@Getter 
+@Setter
+@NoArgsConstructor // 기초생성자
+@AllArgsConstructor
+@ToString
 public class MemberDTO {
 	// 나중에 db랑 연결할 모델들 작성
 	private String memberId;
