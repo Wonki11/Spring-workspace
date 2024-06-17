4669471dcfa14d94771aa35c53dee5bd5c7b0037
Wonki11 <wongi5011@daum.net>
2024-06-17 13:53:17 +0900

    홈페이지 상품등록

---
+++ demoProject3-mybatis1/src/main/java/com/example/demo/DemoProject3Mybatis1Application.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication // 어노테이션 밑에 모두 실행
+public class DemoProject3Mybatis1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoProject3Mybatis1Application.class, args);
+	}
+
+}
---
+++ demoproject3-mybatis-mapper-insert/src/main/java/com/example/demo/dto/Goods.java
@@ -0,0 +1,17 @@
+package com.example.demo.dto;
+
+import lombok.*;
+
+/* lombok활용한 Getter Setter 기초생성자 필수생성자 Tostring 만들기*/
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
+public class Goods {
+	private int goodsId;
+	private String goodsName;
+	private int goodsPrice;
+	private int goodsQuantity;
+
+}
