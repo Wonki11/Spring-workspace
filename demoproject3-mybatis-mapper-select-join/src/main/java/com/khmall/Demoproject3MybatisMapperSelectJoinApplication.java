180e3a954365c20dbbb167391ec6c870c0c5747e
Wonki11 <wongi5011@daum.net>
2024-06-20 11:48:19 +0900

    join까지 써서 연동

---
+++ demoproject3-mybatis-mapper-select-join/src/main/java/com/khmall/Demoproject3MybatisMapperSelectJoinApplication.java
@@ -0,0 +1,13 @@
+package com.khmall;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Demoproject3MybatisMapperSelectJoinApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Demoproject3MybatisMapperSelectJoinApplication.class, args);
+	}
+
+}
